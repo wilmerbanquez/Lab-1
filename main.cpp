89d4a8472c016bd302a34d38c4bd0c7448960361
wilmerbanquez <wilmer.banquez@udea.edu.co>
2021-12-11 12:36:08 -0500

    Lab-1

--- main.cpp
+++ main.cpp
@@ -3,7 +3,7 @@ using namespace std;
 int opcion;
 int main()
 {
-cout <<"Seleccione una opción"<<endl;
+cout <<"Seleccione una opcion"<<endl;
 cout <<"2. para problema 2"<<endl;
 cout <<"4. para problema 4"<<endl;
 cout <<"5. para problema 5"<<endl;
@@ -14,6 +14,7 @@ cout <<"11 para problema 11"<<endl;
 cout <<"13 para problema 13"<<endl;
 cout <<"14 para problema 14"<<endl;
 cout <<"16 para problema 16"<<endl;
+cout <<"0 para salir "<<endl;
 cin >>opcion;
 switch(opcion)
 {
@@ -112,26 +113,98 @@ int num1;
 int num2;
 int hora;
 int minuto;
-int horaf;
 cout <<"Ingrese el primer numero: ";
 cin >>num1;
+while(num1/100 > 24 or num1%100 > 59){
+    cout<<"Ingrese un número válido: "<<endl;
+    cin >>num1;
+}
 cout <<"Ingrese el segundo numero: ";
 cin >>num2;
+while(num2/100 > 24 or num2%100 >59){
+    cout<<"Ingrese un número válido: "<<endl;
+    cin >>num2;
+}
 hora = (num1/100) + (num2/100);
 minuto = (num1%100) + (num2%100);
 if(minuto>60)
 {
     minuto = minuto-60;
     hora = hora + 1;
-    cout <<"La hora es: "<<hora<<minuto;
+    if(hora>24)
+        hora = hora-24;
+    cout <<"La hora es: "<<hora<<minuto<<endl;
 }
 else
 {
- cout <<"La hora es: "<<hora<<minuto<<endl;
+if(hora>=24){
+    hora = hora-24;
+cout <<"La hora es: "<<hora<<minuto<<endl;}
 }
 }
 case 6:
 {
+int num;
+cout<<"Ingrese un numero entero: ";
+cin>>num;
+if(num!=2 and num!=1){
+    num = num-1;
+    float sum = 0;
+    while(num>1){
+        float fact = 1;
+        for(int i=1;i<=num;i++){
+            fact = fact*i;
+        }
+        sum = 1.0/fact + sum;
+        num-=1;
+    }
+    cout<<"e es aproximadamente: "<<sum+2<<endl;
+    num = 123123;
+}
+else
+    if(num==2)
+        cout <<"e es aproximadamente 2"<<endl;
+    if(num==1)
+        cout <<"e es aproximadamente 1"<<endl;
+}
+case 9:
+{
+int num;
+int temp = 0;
+int div = 1;
+int sum = 0;
+cout<<"Ingrese un numero: ";
+cin>>num;
+temp = num;
+while(temp/10 != 0){
+    temp = temp/10;
+    div = div*10;
+}
+cout <<"divisor: "<<div<<endl;
+int aux = div;
+int count = 0;
+while(aux>0){
+    aux = aux/10;
+    count+=1;
+}
+int num1 = num;
+cout <<"cifras: "<<count<<endl;
+while(div>0){
+    num = num1/div;
+    aux = num;
+    if(num==0)
+        num = 1;
+    for(int i=num;i>1;i--){
+        num = num*aux;
+    }
+    sum = num + sum;
+    num1 = num1%div;
+    div = div/10;
+    if(div == 0){
+        cout<<sum<<endl;
+        break;
+    }
+}
 }
 }
 return 0;
